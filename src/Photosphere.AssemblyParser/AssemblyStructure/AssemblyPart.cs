054b349d471e4b053abb9b0983fab8fa883ebd8f
sunloving <ilfirin.nar@gmail.com>
2016-07-31 23:09:59 +0300

    Fix build

--- src/Photosphere.AssemblyParser/AssemblyStructure/AssemblyPart.cs
+++ src/Photosphere.AssemblyParser/AssemblyStructure/AssemblyPart.cs
@@ -15,25 +15,25 @@ namespace Photosphere.AssemblyParser.AssemblyStructure
             ObligatoryValue = obligatoryValue;
         }
 
-        //public AssemblyPart(string name, ushort bytesCount, ushort? bytesCount64 = null)
-        //{
-        //    Name = name;
-        //    BytesCount = bytesCount;
-        //    BytesCount64 = bytesCount64 ?? 0;
-        //}
+        public AssemblyPart(string name, ushort bytesCount, ushort? bytesCount64 = null)
+        {
+            Name = name;
+            BytesCount = bytesCount;
+            BytesCount64 = bytesCount64 ?? 0;
+        }
 
-        //public AssemblyPart(string name, BinarySize bytesCount, BinarySize? bytesCount64 = null)
-        //{
-        //    Name = name;
-        //    BytesCount = (ushort) bytesCount;
-        //    BytesCount64 = (ushort) (bytesCount64 ?? 0);
-        //}
+        public AssemblyPart(string name, BinarySize bytesCount, BinarySize? bytesCount64 = null)
+        {
+            Name = name;
+            BytesCount = (ushort) bytesCount;
+            BytesCount64 = (ushort) (bytesCount64 ?? 0);
+        }
 
         public string Name { get; }
 
-        //public ushort BytesCount { get; }
+        public ushort BytesCount { get; }
 
-        //public ushort BytesCount64 { get; }
+        public ushort BytesCount64 { get; }
 
         public IBinaryStruct ObligatoryValue { get; }
     }
